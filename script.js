4615aea4eb56ebd3cbc1bd64c56c690355d3b4fa
NickT26-Coder <142860763+NickT26-Coder@users.noreply.github.com>
2026-05-27 18:45:06 -0400

    feat: basic etch-a-sketch finished

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 const squareContainer = document.querySelector(".squares-container");
+const changeSizeButton = document.querySelector("button");
 
-let gridDimension = 16;
+createGrid();
 
-for (let i = 0; i < gridDimension; i++) {
-    for (let j = 0; j < gridDimension; j++) {
-        const square = document.createElement("div");
-        square.classList.toggle("square");
-        square.style.width = Math.floor(960 / gridDimension) + "px";
-        square.style.height = square.style.width;
-        squareContainer.appendChild(square);
+function createGrid(gridDimension = 16) {
+    // Different ways to empty a div
+    squareContainer.replaceChildren();
+    for (let i = 0; i < gridDimension; i++) {
+        for (let j = 0; j < gridDimension; j++) {
+            const square = document.createElement("div");
+            square.classList.toggle("square");
+            square.style.width = Math.floor(960 / gridDimension) + "px";
+            square.style.height = square.style.width;
+            squareContainer.appendChild(square);
+        }
     }
 }
+
+squareContainer.addEventListener("mouseover", e => {
+    if (e.target.matches(".square")) {
+        e.target.classList.add("square-hovered");
+    }
+});
+
+changeSizeButton.addEventListener("click", () => {
+    let gridDimension;
+    do {
+        gridDimension = prompt("How many rows of blocks do you want to produce? ");
+    } while (gridDimension > 100 || gridDimension < 1)
+    // gridDimensionY = prompt("How many columns of blocks do you want to produce? ");
+    createGrid(gridDimension);
+})
